e5fbf36d848e5261f587ffc70926c5075775d63a
joe <joebarker@mail.weber.edu>
2019-11-14 21:58:01 -0700

    Form building with vue is nice :)

--- src/App.vue
+++ src/App.vue
@@ -1,181 +1,191 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div id="app">
+        <Switch></Switch>
 
-    <div class="container">
-      <form>
-        <div class="row">
-          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            <h1>File a Complaint</h1>
+        <JoesCom></JoesCom>
+        <div class="container">
+
+            <form>
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <h1>File a Complaint</h1>
+
+
+                      <hr>
+                        <div class="form-group">
+                            <label for="email">Mail</label>
+                            <input
+                                    type="text"
+                                    id="email"
+                                    class="form-control"
+                                    :value="userData.email"
+                                    @input="userData.email = $event.target.value"
+                            >
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input
+                                    type="password"
+                                    id="password"
+                                    class="form-control"
+                                    v-model.lazy="userData.password"
+                            >
+                        </div>
+                        <div class="form-group">
+                            <label for="age">Age</label>
+                            <input
+                                    type="number"
+                                    id="age"
+                                    class="form-control"
+                                    v-model="userData.age"
+                            >
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                        <label for="message">Message</label><br>
+                        <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                        <textarea
+                                id="message"
+                                rows="5"
+                                class="form-control"
+                                v-model="message"
+                        ></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <div class="form-group">
+                            <label for="sendmail">
+                                <input
+                                        type="checkbox"
+                                        id="sendmail"
+                                        value="SendMail"
+                                        v-model="sendMail"
+                                > Send Mail
+                            </label>
+                            <label for="sendInfomail">
+                                <input
+                                        type="checkbox"
+                                        id="sendInfomail"
+                                        value="SendInfoMail"
+                                        v-model="sendMail"
+                                > Send Infomail
+                            </label>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                        <label for="male">
+                            <input
+                                    type="radio"
+                                    id="male"
+                                    value="Male"
+                                    v-model="gender"
+                            > Male
+                        </label>
+                        <label for="female">
+                            <input
+                                    type="radio"
+                                    id="female"
+                                    value="Female"
+                                    v-model="gender"
+                            > Female
+                        </label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                        <label for="priority">Priority</label>
+                        <select
+                                id="priority"
+                                class="form-control"
+                                v-model="selectedPriority"
+                        >
+                            <option v-for="priority in priorities" :key="priority.id"
+                                    :selected="priority === 'Medium'">{{ priority }}
+                            </option>
+                        </select>
+                    </div>
+
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <button
+                                class="btn btn-primary">Submit!
+                        </button>
+                    </div>
+                </div>
+            </form>
             <hr>
-            <div class="form-group">
-              <label for="email">Mail</label>
-              <input
-                      type="text"
-                      id="email"
-                      class="form-control"
-                      v-model="userData.email"
-              >
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4>Your Data</h4>
+                        </div>
+                        <div class="panel-body">
+                            <p>Mail: {{ userData.email }}</p>
+                            <p>Password: {{ userData.password }}</p>
+                            <p>Age: {{ userData.age }}</p>
+                            <p style="white-space: pre">Message: {{ message }} </p>
+                            <p><strong>Send Mail?</strong></p>
+                            <ul>
+                                <li v-for="sendMail in sendMail" :key="sendMail.id">{{ sendMail }}</li>
+                            </ul>
+                            <p>Gender: {{ gender }}</p>
+                            <p>Priority: {{ selectedPriority }}</p>
+                            <p>Switched:</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input
-                      type="password"
-                      id="password"
-                      class="form-control"
-                      v-model.lazy="userData.password"
-              >
-            </div>
-            <div class="form-group">
-              <label for="age">Age</label>
-              <input
-                      type="number"
-                      id="age"
-                      class="form-control"
-                      v-model="userData.age"
-              >
-            </div>
-
-          </div>
         </div>
-        <div class="row">
-          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-            <label for="message">Message</label><br>
-            <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-            <textarea
-                    id="message"
-                    rows="5"
-                    class="form-control"
-                    v-model="message"
-            ></textarea>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            <div class="form-group">
-              <label for="sendmail">
-                <input
-                        type="checkbox"
-                        id="sendmail"
-                        value="SendMail"
-                        v-model="sendMail"
-                > Send Mail
-              </label>
-              <label for="sendInfomail">
-                <input
-                        type="checkbox"
-                        id="sendInfomail"
-                        value="SendInfoMail"
-                        v-model="sendMail"
-                > Send Infomail
-              </label>
-            </div>
-
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-            <label for="male">
-              <input
-                      type="radio"
-                      id="male"
-                      value="Male"
-                      v-model="gender"
-              > Male
-            </label>
-            <label for="female">
-              <input
-                      type="radio"
-                      id="female"
-                      value="Female"
-                      v-model="gender"
-              > Female
-            </label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
-            <label for="priority">Priority</label>
-            <select
-                    id="priority"
-                    class="form-control"
-                    v-model="selectedPriority"
-            >
-              <option v-for="priority in priorities" :key="priority.id"
-                      :selected="priority === 'Medium'">{{ priority }}
-              </option>
-            </select>
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            <button
-                    class="btn btn-primary">Submit!
-            </button>
-          </div>
-        </div>
-      </form>
-      <hr>
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h4>Your Data</h4>
-            </div>
-            <div class="panel-body">
-              <p>Mail: {{ userData.email }}</p>
-              <p>Password: {{ userData.password }}</p>
-              <p>Age: {{ userData.age }}</p>
-              <p style="white-space: pre">Message: {{ message }} </p>
-              <p><strong>Send Mail?</strong></p>
-              <ul>
-                <li v-for="sendMail in sendMail" :key="sendMail.id">{{ sendMail }}</li>
-              </ul>
-              <p>Gender: {{ gender }}</p>
-              <p>Priority: {{ selectedPriority }}</p>
-              <p>Switched:</p>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 
-export default {
-  name: 'app',
+    import Switch from "./components/Switch";
+    import JoesCom from "./components/JoesCom";
 
-  data:() => ({
-    userData: {
-      email: '',
-      password: '',
-      age: 31
+    export default {
+        name: 'app',
 
-    },
-    email:'',
-    message:'',
-    gender: 'Female',
-    selectedPriority: 'High',
-    priorities: ['High', 'Medium', 'Low'],
-    sendMail: []
+        data: () => ({
+            userData: {
+                email: '',
+                password: '',
+                age: 31
 
-  }),
-  components: {
+            },
+            email: '',
+            message: '',
+            gender: 'Female',
+            selectedPriority: 'High',
+            priorities: ['High', 'Medium', 'Low'],
+            sendMail: []
 
-  }
-}
+        }),
+        components: {
+            Switch, JoesCom
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
